c9762356251c064fd3486bb59cc399c265a7386d
rmbc17 <ricardo.carvalhal@hotmail.com>
2024-09-23 11:22:12 +0200

    commit 2

--- 23.09.2024.py
+++ 23.09.2024.py
@@ -1,1 +1,25 @@
-a = ("LuisEgay")
+import streamlit as st
+
+a = ("Benfica.jpeg")
+b = ("Benfica2.jpeg")
+c = ("Benfica3.jpeg")
+d = ("Benfica4.jpeg")
+e = ("Herunterladen.jpeg")
+f = ("OIP.jpeg")
+
+col1, col2, col3 = st.columns (3)
+col4, col5, col6 = st.columns (3)
+
+with col1:
+    st.image(a)
+with col2:
+    st.image(b)
+with col3:
+    st.image(c)
+with col4:
+    st.image(d)
+with col5:
+    st.image(e)
+with col6:
+    st.image(f)
+
